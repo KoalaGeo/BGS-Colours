092dab274dc9fcb9976cb176838ee7cd9efb4a20
koalageo <eddlewis85@gmail.com>
2020-03-31 20:29:58 +0100

    Update

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,16 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo': 'logo.jpeg',
+    'github_user': 'koalageo',
+    'github_repo': 'yearbook-test',
+}
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = 'images/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
