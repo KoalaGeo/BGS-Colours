e589355f161bdc3e6b1d69cbf4ba1a317360dbfa
KoalaGeo <KoalaGeo@users.noreply.github.com>
2020-04-02 13:22:54 +0000

    Automated deployment: Thu Apr  2 13:22:54 UTC 2020 c4be3541d2c857e7b34f184cce98dcae1edbcf9b

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     // Handler when the DOM is fully loaded
     console.log("hey")
 
-    var table = new Tabulator("#bauxite-table", {});
+    var table = new Tabulator("#bauxite-table", {layout:"fitColumns",});
     var table = new Tabulator("#alumina-table", {});
    
 });
